6a443202a4a2d49a7a4ae45a4c0170b07b6563a1
Leo Bjerner <leonard.bjerner@gmail.com>
2026-04-01 10:51:53 +0200

    added v1 with data and some styling

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,23 @@
-import { ingredients, potions, recipes } from "./data.ts";
-
+import { ingredients, potions, recipes } from "./data.js";
 const containers = document.querySelectorAll(".potionContainer");
-
 potions.forEach((potion, index) => {
     const container = containers[index];
-    if (!container) return;
-
+    if (!container)
+        return;
     container.textContent = potion.name;
 });
-
 containers.forEach((container, index) => {
     container.addEventListener("click", () => showPotion(index));
 });
-
 const recipeContainer = document.querySelector("#recipeContainer");
-
-function showPotion(index: number): void {
+function showPotion(index) {
     const potion = potions[index];
     const recipe = recipes.find(r => r.result === potion?.id);
-    if (!potion || !recipe || !recipeContainer) return;
-
+    if (!potion || !recipe || !recipeContainer)
+        return;
     const recipeIngredients = recipe.ingredients.map(id => {
         const ingredient = ingredients.find(i => i.id === id);
         return `${ingredient?.name}(${ingredient?.element})`;
     });
-
     recipeContainer.textContent = recipeIngredients.join(", ");
 }
